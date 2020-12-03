cf677aac8197722b17f1b9f997611cfeb86c4303
Davide <kalelc351k@gmail.com>
2020-12-03 15:01:02 +0100

    Milestone 1

--- js/script.js
+++ js/script.js
@@ -1,6 +1,26 @@
 var app = new Vue({
     el: '#root',
     data: {
-        
+        // https://api.themoviedb.org/3/search/movie/?api_key=aa17a945ada5e90bc96db53bad6edbc1&query=
+        apiFilm: 'https://api.themoviedb.org/3/search/movie/?api_key=',
+        key: 'aa17a945ada5e90bc96db53bad6edbc1',
+        inputCerca: '',
+        arrayFilm: [],
+        arrayTv: [],
+    },
+    mounted: function() {
+
+    },
+    methods: {
+        filtra() {
+            const chiamata = axios.get(this.apiFilm + this.key + '&language=it_IT&query=' + this.inputCerca);
+            chiamata.then(risposta => {
+                var linkQuery = risposta.data.results;
+                this.arrayFilm = linkQuery;
+                this.inputCerca = '';
+            });
+
+            console.log(this.arrayFilm);
+        }
     }
 })
