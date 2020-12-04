0fce072c36eb1415b2ce9f4078b4ac0295e980c2
Davide <kalelc351k@gmail.com>
2020-12-04 16:59:56 +0100

    aggiunta serie tv

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ var app = new Vue({
     data: {
         // https://api.themoviedb.org/3/search/movie/?api_key=aa17a945ada5e90bc96db53bad6edbc1&query=
         apiFilm: 'https://api.themoviedb.org/3/search/movie/?api_key=',
+        apiTv: 'https://api.themoviedb.org/3/search/tv?api_key=',
         key: 'aa17a945ada5e90bc96db53bad6edbc1',
         inputCerca: '',
         arrayFilmLocal: [],
@@ -14,19 +15,34 @@ var app = new Vue({
 
     },
     methods: {
-        filtra() {
-            const chiamata = axios.get(this.apiFilm + this.key + '&language=it_IT&query=' + this.inputCerca);
-            chiamata.then(risposta => {
-                var arrayFilmApi = risposta.data.results;
+        // filtra() {
+        //     const chiamataFilm = axios.get(this.apiFilm + this.key + '&language=it_IT&query=' + this.inputCerca);
+        //     chiamataFilm.then(risposta => {
+        //         var arrayFilmApi = risposta.data.results;
 
-                var filmModificati = arrayFilmApi.map((item) => {
-                    console.log('singolo film', item);
+        //         var filmModificati = arrayFilmApi.map((item) => {
+        //             console.log('singolo film', item);
+        //             item.numeroStars = Math.round(item.vote_average / 2);
+        //             item.bandiera = 'img/' + item.original_language + '.svg';
+                    
+        //             return item;
+        //         });
+        //             this.arrayFilmLocal = filmModificati;
+        //             this.inputCerca = '';
+        //     });
+        // },
+        filtra() {
+            const chiamataTv = axios.get(this.apiTv + this.key + '&language=it_IT&query=' + this.inputCerca);
+            chiamataTv.then(risposta => {
+                var arrayTvApi = risposta.data.results;
+
+                var tvModificati = arrayTvApi.map((item) => {
                     item.numeroStars = Math.round(item.vote_average / 2);
                     item.bandiera = 'img/' + item.original_language + '.svg';
-                    
+
                     return item;
                 });
-                    this.arrayFilmLocal = filmModificati;
+                    this.arrayTvLocal = tvModificati;
                     this.inputCerca = '';
             });
         },
